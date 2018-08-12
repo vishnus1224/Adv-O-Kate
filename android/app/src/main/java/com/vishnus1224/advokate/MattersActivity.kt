6d1b024b505c396eaa23d30893bf8e550102ff49
Vishnu Shirodkar <vishnu.shirodkar@daresay.co>
2018-08-12 13:31:10 +0200

    empty project

---
+++ android/app/src/main/java/com/vishnus1224/advokate/MattersActivity.kt
@@ -0,0 +1,12 @@
+package com.vishnus1224.advokate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MattersActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_matters)
+  }
+}
